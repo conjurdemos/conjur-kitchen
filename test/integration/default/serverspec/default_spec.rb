02a8684c973a3fa40ca902fdf20623b57844d3c8
Dustin Collins <dustinmm80@users.noreply.github.com>
2014-11-24 16:32:35 -0500

    NOT set

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'rabbitmq_with_conjur' do
-    it 'should set the user/pass as default guest:guest' do
+    it 'should not set the user/pass as default guest:guest' do
         rabbitmq_config = file('/etc/rabbitmq/rabbitmq.config')
 
         expect(rabbitmq_config).to_not contain 'guest'
